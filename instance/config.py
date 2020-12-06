958c09fb177597997b05a678e995c2acb55b5e38
Pablo Dominguez Villacanas <pablo.dominguez@situm.com>
2020-12-06 17:24:43 +0100

    Adding instances configuration for flask

---
+++ instance/config.py
@@ -0,0 +1,34 @@
+FLASK_APP = 'tinder_insights'
+LOGGING_CONFIG = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'standard': {
+            'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
+        },
+    },
+    'handlers': {
+        'default': {
+            'level': 'INFO',
+            'formatter': 'standard',
+            'class': 'logging.StreamHandler',
+            'stream': 'ext://sys.stdout',
+        },
+    },
+    'loggers': {
+        '': {  # root logger
+            'handlers': ['default'],
+            'level': 'WARNING',
+            'propagate': True
+        },
+        'tinder_insights': {
+            'level': 'INFO',
+            'handlers': ['default'],
+            'propagate': True
+        },
+        'apscheduler': {
+            'level': 'WARNING',
+            'propagate': True
+        },
+    }
+}
